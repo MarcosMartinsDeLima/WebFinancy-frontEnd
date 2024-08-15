0266297feb1ae352be5c27fff7e0347020b59038
MarcosMartinsDeLima <0timetraveler@gmail.com>
2024-08-14 21:52:34 -0300

    feat: adicionando inicio das pages

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Home.module.css'
+
+export const Home = () => {
+  return (
+    <div className={styles.mainContainer}>
+        <h1>Ja pensou em administrar seus gastos e ganhos?</h1>
+        <p>Com a Web<span>Financy</span> isso é possivel, você pode controlar todos os seus gastos, anotando todos gastos e receitas, e com isso receber um balanço geral</p>
+    </div>
+  )
+}
