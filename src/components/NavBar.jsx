cf098315016971f3f8d169c4d2bdca50e4ee93c0
MarcosMartinsDeLima <0timetraveler@gmail.com>
2024-08-15 17:28:10 -0300

    feat: adicionando rota de  dasboard

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 import styles from './NavBar.module.css'
+import imagem from '../assets/dashboard.png'
 
 export const NavBar = () => {
   return (
@@ -14,6 +15,11 @@ export const NavBar = () => {
             </li>
             <li>
                 <NavLink to={'/user/login'} className={styles.entrar}>Entrar</NavLink>
+            </li>
+            <li>
+              <NavLink to={'/user/dashboard'}>
+                <img src={imagem} alt="dashboard" />
+              </NavLink>  
             </li>    
         </ul>
     </div>
